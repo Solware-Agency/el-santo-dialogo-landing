38861cccedda5643ff23de6ce18afa9de6138306
Solware <solwareve@gmail.com>
2025-09-28 12:48:51 -0400

    Added vite.config.ts.timestamp-1759078130957-86d7ac97b62158.mjs

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,24 +1,5 @@
 // Centralized icon imports and mapping
-import { 
-  BookOpen, 
-  Stethoscope, 
-  Cross, 
-  User, 
-  MapPin, 
-  Calendar,
-  Phone, 
-  Mail, 
-  Instagram, 
-  Facebook, 
-  Youtube,
-  Menu,
-  X,
-  Image,
-  FileText,
-  HandHeart,
-  Users,
-  Code2
-} from "lucide-react";
+import { BookOpen, Stethoscope, Cross, User, MapPin, Calendar, Phone, Mail, Instagram, Facebook, Youtube, Menu, X, Image, FileText, HandHeart, Users, Code as Code2 } from "lucide-react";
 
 export const ICONS = {
   BookOpen,
